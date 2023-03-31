8fa972e67a3600aaad21b0f6c649a3770f570696
MatthewKK01 <mateyavelashvili@gmail.com>
2023-03-31 10:16:22 +0400

    added Features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Features from "./components/Features";
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Features />
     </div>
   );
 }
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,7 +1,42 @@
 import React from "react";
-
+import "./Features.css";
+import spa from "../images/spa.png";
+import hair from "../images/hair.jpg";
+import feet from "../images/feet.jpg";
 function Features() {
-  return <div>Features</div>;
+  return (
+    <section className="px-[360px] py-[151px]">
+      <div className="container grid grid-cols-3 m-auto gap-28">
+        <div>
+          <img src={spa} alt="" />
+          <h1>Spa & Massage</h1>
+          <p>
+            If you are going to use a passage offer Lorem Ipsum, you need to be
+            sure hidden in the middle of text.
+          </p>
+          <button>Read More</button>
+        </div>
+        <div>
+          <img src={hair} alt="" />
+          <h1>Spa & Massage</h1>
+          <p>
+            If you are going to use a passage offer Lorem Ipsum, you need to be
+            sure hidden in the middle of text.
+          </p>
+          <button>Read More</button>
+        </div>
+        <div>
+          <img src={feet} alt="" />
+          <h1>Spa & Massage</h1>
+          <p>
+            If you are going to use a passage offer Lorem Ipsum, you need to be
+            sure hidden in the middle of text.
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Features;
